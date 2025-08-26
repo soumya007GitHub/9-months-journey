13d8e4365ace4259e2987cc69d5dc52546bb181e
soumya007GitHub <130821564+soumya007GitHub@users.noreply.github.com>
2025-08-26 22:53:55 +0530

    Added sum, comments java files

---
+++ 002 - Day 2/Comments.java
@@ -0,0 +1,12 @@
+public class Comments {
+    public static void main(String[] args) {
+        // This is a single-line comment
+
+        /*
+         This is a multi-line comment
+         It can span multiple lines
+        */
+
+        System.out.println("Hello, World!"); // Print greeting to the console
+    }
+}
