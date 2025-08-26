1f03a57c1ce9c0e414a999898cf9563e5ed241e7
soumya007GitHub <130821564+soumya007GitHub@users.noreply.github.com>
2025-08-26 20:35:30 +0530

    Added java basic files

---
+++ 002 - Day 2/JavaOutput.java
@@ -0,0 +1,8 @@
+public class JavaOutput {
+    public static void main(String[] args) {
+        System.out.print("This is way 1 of printing");
+        System.out.println("");
+        System.out.println("This is way 2 for printing with a new line");
+        System.out.print("This is another way to print things on screen \nwith new line");
+    }
+}
