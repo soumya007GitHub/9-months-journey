4799b85a8a23dbf61081f19f89544290a82784e3
soumya007GitHub <130821564+soumya007GitHub@users.noreply.github.com>
2025-08-26 22:46:51 +0530

    Added Datatypes file from java

---
+++ 002 - Day 2/Datatypes.java
@@ -0,0 +1,20 @@
+public class Datatypes{
+    public static void main(String[] args){
+        byte a = 1;
+        short b = 300;
+        char c = 'A';
+        int d = 1200;
+        long e = 123456789L;
+        float f = 10.5f;
+        double g = 20.99;
+        boolean h = true;
+        System.out.println("Byte Size:"+1+" byte");
+        System.out.println("Short Size:"+2+" bytes");
+        System.out.println("Char Size:"+2+" bytes");
+        System.out.println("Int Size:"+4+" bytes");
+        System.out.println("Long Size:"+8+" bytes");    
+        System.out.println("Float Size:"+4+" bytes");
+        System.out.println("Double Size:"+8+" bytes");
+        System.out.println("Boolean Size:"+1+" byte");
+    }
+}
