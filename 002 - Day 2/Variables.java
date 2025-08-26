6cb20acb0fd9a404aef25545bb79c707040ff34a
soumya007GitHub <130821564+soumya007GitHub@users.noreply.github.com>
2025-08-26 20:54:49 +0530

    Added variables intro file

---
+++ 002 - Day 2/Variables.java
@@ -0,0 +1,10 @@
+public class Variables{
+    public static void main(String[] args){
+        int num = 10;
+        String name = "Soumya";
+        System.out.println("Number: " + num);
+        System.out.println("String: " + name);
+        System.out.println("Here int, String are datatypes, num and name are variables");
+        System.out.println("In memory a certian space will be occupied the value and can identified by using identifiers like num and name");
+    }
+}
