2fded6090a8755f213f297ae1bdafa0e01a6397c
soumya007GitHub <130821564+soumya007GitHub@users.noreply.github.com>
2025-08-31 20:20:54 +0530

    Accessing variable out of scope example

---
+++ 005 - Day 5/Error.java
@@ -0,0 +1,8 @@
+public class Error {
+    public static void main(String[ ] args){
+        for(int i = 0; i<5; ++i){
+            System.out.println(i);
+        }
+        System.out.println("Value of i after the loop: "+i);;
+    }
+}
