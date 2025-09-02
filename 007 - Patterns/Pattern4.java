b73359e1915a8c4c7b6d4dc9f94be3365715b6e6
soumya007GitHub <130821564+soumya007GitHub@users.noreply.github.com>
2025-09-02 21:58:42 +0530

    Added 4 different ways to print same pattern in Pattern4.java

---
+++ 007 - Patterns/Pattern4.java
@@ -0,0 +1,32 @@
+public class Pattern4 {
+    public static void main(String[] args) {
+        // Approach 1
+        // for(int i = 1; i<=4; ++i){
+        //     for(int j = 1; j<=4-i+1; ++j){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+        // Approach 2
+        // for(int i = 0; i<4; ++i){
+        //     for(int j = 1; j<=4-i; ++j){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+        // Approach 3
+        // for(int i = 1; i<=4; ++i){
+        //     for(int j = 0; j<=4-i; ++j){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+        // Approach 4
+        // for(int i = 4; i>0; --i){
+        //     for(int j = 1; j<=i; ++j){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+    }
+}
---
+++ 007 - Patterns/Readme.md
@@ -0,0 +1,5 @@
+in pattern related questions first we will identify how many number of rows we have and based on that we will have value of N
+inside each row we will check how many number of stars we have or number of space we have
+based on those pattern on each row we will design a code with nested loop
+
+Covered 4 different ways to print the same pattern in Pattern4.java
