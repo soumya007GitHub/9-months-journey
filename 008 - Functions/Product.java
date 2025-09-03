a84a2742183a8b1e71d182f462b55b1e5cb13280
soumya007GitHub <130821564+soumya007GitHub@users.noreply.github.com>
2025-09-03 22:28:04 +0530

    Added product function

---
+++ 008 - Functions/Product.java
@@ -0,0 +1,11 @@
+public class Product {
+    public static int product(int a, int b){
+        return a*b;
+    }
+    public static void main(String[] args){
+        int a = 10;
+        int b = 2;
+        int productValue = product(a, b);
+        System.out.println(productValue);
+    }
+}
