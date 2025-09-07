2071824e11f047293a1db2c0d2c327fd4c92290f
soumya007GitHub <130821564+soumya007GitHub@users.noreply.github.com>
2025-09-07 22:29:05 +0530

    Added code for largest, smallest element from an array

---
+++ 010 - Arrays/LargestSmallest.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class LargestSmallest {
+
+    public static int largestNumberFunc(int numbers[]){
+        int largest = Integer.MIN_VALUE;
+        for(int i = 0; i<numbers.length; ++i){
+            if(numbers[i] > largest){
+                largest = numbers[i];
+            }
+        }
+        return largest;
+    }
+
+    public static int smallestNumberFunc(int numbers[]){
+        int smallest = Integer.MAX_VALUE;
+        for(int i = 0; i<numbers.length; ++i){
+            if(numbers[i] < smallest){
+                smallest = numbers[i];
+            }
+        }
+        return smallest;
+    }
+    public static void main(String args[]){
+        int numbers[] = {56, 34, 89, 67, 88, 100, 43};
+        int largestNumber = largestNumberFunc(numbers);
+        int smallestNumber = smallestNumberFunc(numbers);
+        System.out.println("Largest : "+largestNumber);
+        System.out.println("Smallest : "+smallestNumber);
+    }
+}
