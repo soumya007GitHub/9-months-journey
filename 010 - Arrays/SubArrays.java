37ac5decfaaa72b292ee0dfd4bc678fd494218f7
soumya007GitHub <130821564+soumya007GitHub@users.noreply.github.com>
2025-09-08 09:53:27 +0530

    Addec sub-arrays question code

---
+++ 010 - Arrays/SubArrays.java
@@ -0,0 +1,18 @@
+public class SubArrays {
+    public static void main(String args[]){
+        int nums[] = {10, 20, 30, 40, 50};
+        for(int i = 0; i<nums.length; ++i){
+            for(int j = i; j<nums.length; ++j){
+                System.out.print("[");
+                for(int k = i; k<=j; ++k){
+                    System.out.print(nums[k]);
+                    if(k < j){
+                        System.out.print(", ");
+                    }
+                }
+                System.out.print("] ");
+            }
+            System.out.println();
+        }
+    }
+}
