2b9b183cec023769ea84eae66accadca70d46bae
soumya007GitHub <130821564+soumya007GitHub@users.noreply.github.com>
2025-09-16 09:49:52 +0530

    Added code for string length and concatenation

---
+++ 015 - Strings/Additional.java
@@ -0,0 +1,9 @@
+public class Additional {
+    public static void main(String args[]){
+        String fname = "Soumya";
+        String lname = "Tripathy";
+        String fullname = fname+" "+lname;
+        System.out.println(fullname.length());
+        System.out.println(fullname.charAt(5));
+    }
+}
