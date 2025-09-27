2da240a19b795e0af0c59bad8afd9bc68ee36a70
soumya007GitHub <130821564+soumya007GitHub@users.noreply.github.com>
2025-09-27 15:44:12 +0530

    Added factorial code for recursion

---
+++ 019 - Recursion 1/Factorial.java
@@ -0,0 +1,14 @@
+public class Factorial {
+
+    public static int factorial(int n){
+        if(n == 0){
+            return 1;
+        }
+        return n * factorial(n - 1);
+    }
+
+    public static void main(String args[]){
+        int num = 5;
+        System.out.println(factorial(num));
+    }
+}
