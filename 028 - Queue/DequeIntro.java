bde488332cfb815896aa7c873114f042bb423175
soumya007GitHub <soumyatripathy2021@gmail.com>
2025-10-17 01:52:30 +0530

    Added code for Deque - double ended queue

---
+++ 028 - Queue/DequeIntro.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class DequeIntro{
+    public static void main(String args[]){
+        Deque<Integer> d = new LinkedList<>();
+        d.addFirst(10);
+        d.addFirst(20);
+        d.addLast(30);
+        d.addLast(40);
+        d.addLast(50);
+        System.out.println(d);
+        d.removeFirst();
+        System.out.println(d);
+        System.out.println(d.peek());
+        d.removeLast();
+        System.out.println(d);
+    }
+}
