52a76c5ade36343fa75cf7cb09545898dce12e75
soumya007GitHub <soumyatripathy2021@gmail.com>
2025-10-17 01:22:27 +0530

    Added code for interleave

---
+++ 028 - Queue/InterleaveTwoHalves.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class InterleaveTwoHalves {
+
+    public static void interleave(Queue<Integer> q){
+        int size = q.size()/2;
+
+        Queue<Integer> q2 = new LinkedList<>();
+        for(int i = 0; i<size; ++i){
+            q2.add(q.remove());
+        }
+        while(!q2.isEmpty()){
+            q.add(q2.remove());
+            q.add(q.remove());
+        }
+    }
+    public static void main(String args[]){
+        Queue<Integer> q = new LinkedList<>();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+        q.add(5);
+        q.add(6);
+        interleave(q);
+
+        while(!q.isEmpty()){
+            System.out.print(q.remove()+" ");
+        }
+    }
+}
