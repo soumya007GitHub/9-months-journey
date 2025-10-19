7873385b9e982b9c8f34a73148ea52509bbd3b2e
soumya007GitHub <soumyatripathy2021@gmail.com>
2025-10-19 18:15:36 +0530

    Total Activities completed in n time

---
+++ 029 - Greedy/Activity.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Activity {
+    public static void main(String args[]){
+        int start[] = {1, 3, 0, 5, 8, 5};
+        int end[] = {2, 4, 6, 7, 9, 9};
+
+        int count = 1;
+        ArrayList<Integer> activities = new ArrayList<>();
+        activities.add(0);
+        int lastactivity = end[0];
+        for(int i = 1; i<end.length; ++i){
+            if(start[i] >= lastactivity){
+                activities.add(i);
+                lastactivity = end[i];
+                ++count;
+            }
+        }
+        for(int i = 0; i<activities.size(); ++i){
+            System.out.println(start[activities.get(i)]+", "+end[activities.get(i)]);
+        }
+    }
+}
