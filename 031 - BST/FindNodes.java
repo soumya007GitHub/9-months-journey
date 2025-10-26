7a3464a2a5cfe3434d40e1aae977a2194ad5c998
soumya007GitHub <soumyatripathy2021@gmail.com>
2025-10-27 00:02:28 +0530

    correct code for BST nodes range search

--- 031 - BST/FindNodes.java
+++ 031 - BST/FindNodes.java
@@ -1,22 +1,23 @@
-import java.util.*;
+
 class FindNodes{
     
     public ArrayList<Integer> array = new ArrayList<>();
     public ArrayList<Integer> printNearNodes(Node root, int low, int high) {
         traverse(root, low, high);
-        Collections.sort(array);
         return array;
     }
     public void traverse(Node root, int low, int high){
         if(root == null){
             return;
         }
+        if(root.data >= low){
+            traverse(root.left, low, high);
+        }
         if(root.data >= low && root.data<=high){
             array.add(root.data);
-        }else{
-            return;
         }
-        traverse(root.left, low, high);
-        traverse(root.right, low, high);
+        if(root.data <= high){
+            traverse(root.right, low, high);
+        }
     }
 }
